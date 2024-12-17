5a8b89de9fbf0ffa57b12ed8a66f6d02370afcc2
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-12-17 10:58:48 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,22 +2,30 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	// Read the value of lines from command-line flag.
+	lines := flag.Bool("l", false, "Count lines")
+	flag.Parse()
+
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// Declare a scanner to read from io.Reader
 	scanner := bufio.NewScanner(r)
 
-	// Split the data into words, default is by lines.
-	scanner.Split(bufio.ScanWords)
-
+	// Check if countLines is false, which in this case count
+	// the words.
+	if !countLines {
+		// Split the data into words, default is by lines.
+		scanner.Split(bufio.ScanWords)
+	}
 	wc := 0
 
 	for scanner.Scan() {
