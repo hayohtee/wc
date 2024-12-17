4f34e61d91d0a732d1ad2bf6c8cd7bba05a301d2
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-12-17 11:02:57 +0100

    Update main_test.go
    Update TestCountWords function
    Add TestCountLines function

--- main_test.go
+++ main_test.go
@@ -9,7 +9,18 @@ func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
 
-	got := count(b)
+	got := count(b, false)
+
+	if got != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, got)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
+	exp := 3
+
+	got := count(b, true)
 
 	if got != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, got)
