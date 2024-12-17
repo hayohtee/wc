5c16a7248de36b4420a6a17891576349436650db
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-12-17 11:31:47 +0100

    Update main_test.go
    Update TestCountWords function
    Update TestCountLines function
    Add TestCountBytes function
    Add TestCountLinesBytesPrecedence

--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
 
-	got := count(b, false)
+	got := count(b, false, false)
 
 	if got != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, got)
@@ -20,8 +20,28 @@ func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
 	exp := 3
 
-	got := count(b, true)
+	got := count(b, true, false)
 
+	if got != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, got)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("word1, word2, word3 word4")
+	exp := 25
+
+	got := count(b, false, true)
+	if got != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, got)
+	}
+}
+
+func TestCountLinesBytesPrecedence(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
+	exp := 35
+
+	got := count(b, true, true)
 	if got != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, got)
 	}
