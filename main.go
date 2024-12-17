6ad48d5e8d492f02e0d15bf2135206595d87ddab
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-12-17 11:24:04 +0100

    Update  main.go
    Include counting by bytes

--- main.go
+++ main.go
@@ -10,13 +10,17 @@ import (
 
 func main() {
 	// Read the value of lines from command-line flag.
-	lines := flag.Bool("l", false, "Count lines")
+	countLines := flag.Bool("l", false, "Count lines")
+	countBytes := flag.Bool("b", false, "Count bytes")
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *countLines, *countBytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+// count is a function that reads a value from the standard input
+// (STDIN) and counts the number of lines, words, as specified. With
+// count bytes taking precedence if specified.
+func count(r io.Reader, countLines, countBytes bool) int {
 	// Declare a scanner to read from io.Reader
 	scanner := bufio.NewScanner(r)
 
@@ -26,6 +30,12 @@ func count(r io.Reader, countLines bool) int {
 		// Split the data into words, default is by lines.
 		scanner.Split(bufio.ScanWords)
 	}
+
+	// If countBytes is true, then split the scanner by bytes.
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	}
+
 	wc := 0
 
 	for scanner.Scan() {
